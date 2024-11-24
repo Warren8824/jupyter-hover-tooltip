0d1016e2f89686b367d126e041013e0f6b66e4ef
Warren8824 <warrenbebbington88@gmail.com>
2024-11-24 14:26:31 +0000

    refactor project.

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name="jupyter_hover_tooltip",
     version="0.1.0",
-    packages=["jupyter_hover_tooltip"],
+    packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
     data_files=[
         ("share/jupyter/labextensions/jupyter-hover-tooltip", [
             "lib/index.js",
-            "package.json"
+            "lib/index.js.map",
+            "package.json",
+            "schema/plugin.json"
         ]),
     ],
     install_requires=[
         "jupyterlab>=4.0.0"
     ],
+    python_requires=">=3.8",
+    zip_safe=False,
 )

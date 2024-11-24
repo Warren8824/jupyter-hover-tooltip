dee1275439ceb7a1a1b93e1ec711ff73c17d9d97
Warren8824 <warrenbebbington88@gmail.com>
2024-11-24 15:22:57 +0000

    update index.js with log print.

--- src/jupyter_hover_tooltip/static/index.js
+++ src/jupyter_hover_tooltip/static/index.js
@@ -6,17 +6,22 @@ const plugin = {
     autoStart: true,
     requires: ['@jupyterlab/notebook'],
     activate: function(app, notebookTracker) {
+        console.log('Jupyter hover tooltip extension is activated!'); // Debug message
         const tooltipManager = new TooltipManager();
 
         notebookTracker.widgetAdded.connect((sender, notebook) => {
+            console.log('Notebook widget added'); // Debug message
             notebook.content.rendered.connect(() => {
+                console.log('Notebook rendered'); // Debug message
                 const cells = notebook.content.widgets;
                 cells.forEach(cell => {
                     if (cell.model.type === 'markdown') {
+                        console.log('Found markdown cell'); // Debug message
                         const element = cell.node;
                         const links = element.getElementsByTagName('a');
 
                         Array.from(links).forEach(link => {
+                            console.log('Found link:', link.href); // Debug message
                             link.addEventListener('mouseover', (e) => {
                                 const href = link.getAttribute('href');
                                 if (href && href.startsWith('#')) {
